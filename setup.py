74675267f2761457e7558d3df40f5d88091da029
Seth Kerr <41877068+skerr92@users.noreply.github.com>
2019-05-14 08:08:47 -0400

    Update setup.py
    adding basic structure to setup file.

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
 # setup stuff is going here
+
+from setuptools import setup
+
+setup(
+  name='oakaipy',
+  version='0.0.1',
+  description='Analytical tools for financial, mathmatical, and scientific computation and prediciton with Artificial Intelligence',
+  license='MIT',
+  packages=['oakaipy'],
+  author='Seth Kerr',
+  author_email='seth.kerr.oaktech@gmail.com',
+  keywords=['ai','analytics','artificial intelligence','prediction tools'],
+  url='https://github.com/skerr92/oakaipy'
+)
